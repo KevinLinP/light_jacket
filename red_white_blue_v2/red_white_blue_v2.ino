3efdc1be1255fd6fd9d5910ae9ccbb6eb4ef3cf2
Kevin Lin <kevin.lin.p@gmail.com>
2015-07-04 09:35:46 -0700

    red/white/blue: v2, added pot. controls for brightness, lifetime, and flash rate.

--- red_white_blue_v2/red_white_blue_v2.ino
+++ red_white_blue_v2/red_white_blue_v2.ino
@@ -5,13 +5,10 @@ const byte LEDS = 50;
 
 const byte FRAMES_PER_SECOND = 60;
 const long INTERVAL = 1000 / FRAMES_PER_SECOND;
-const float MAX_BRIGHTNESS = 0.125;
 
 const byte RED = 0;
 const byte WHITE = 1;
 const byte BLUE = 2;
-const byte MIN_LIFETIME = 2 * FRAMES_PER_SECOND;
-const byte MAX_LIFETIME = 3 * FRAMES_PER_SECOND;
 
 byte FULL_RED[] = {255, 0, 0};
 byte FULL_WHITE[] = {255, 255, 255};
@@ -25,11 +22,16 @@ byte ledLifeLeft[LEDS];
 
 void setup() {
   TCL.begin();
+  TCL.setupDeveloperShield();
+
+  byte lifetimeMultiplier = getLifetimeMultiplier();
+  byte minLifetime = 2 * lifetimeMultiplier;
+  byte maxLifetime = 3 * lifetimeMultiplier;
 
   byte i;
   for(i = 0; i < LEDS; i++) {
     ledColor[i] = random(0, 3);
-    byte lifetime = random(MIN_LIFETIME, MAX_LIFETIME);
+    byte lifetime = random(minLifetime, maxLifetime);
     ledLifetime[i] = lifetime;
     ledLifeLeft[i] = random(0, lifetime);
   }
@@ -37,22 +39,32 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
+
+
   
   if (currentMillis - previousMillis > INTERVAL) {
     previousMillis = currentMillis;
-    TCL.sendEmptyFrame();
+    
+    float maxBrightness = (analogRead(TCL_POT4) >> 2) / 255.0;
+    long randomRate = pow(analogRead(TCL_POT1), 1.5) + 1;
 
+    byte lifetimeMultiplier = getLifetimeMultiplier();
+    byte minLifetime = 2 * lifetimeMultiplier;
+    byte maxLifetime = 3 * lifetimeMultiplier;
+    
+    TCL.sendEmptyFrame();
+    
     byte i;
     for(i = 0; i < LEDS; i++) {
       byte color = ledColor[i];
       byte lifeLeft = ledLifeLeft[i];
 
-      if (random(100000) < 125) {
-        float brightness = random(50,101) / 100.0;
+      if (random(100000) < randomRate) {
+        float brightness = random(50, 101) / 100.0;
         sendColorAndBrightness(FULL_WHITE, brightness);
       } else {
-        float brightness = 1.0 * lifeLeft  / ledLifetime[i] * MAX_BRIGHTNESS;
-
+        float brightness = 1.0 * lifeLeft  / ledLifetime[i] * maxBrightness;
+  
         switch(color) {
         case RED :
         sendColorAndBrightness(FULL_RED, brightness);
@@ -68,7 +80,7 @@ void loop() {
 
       if (lifeLeft == 0) {
         ledColor[i] = (color + random(1, 3)) % 3;
-        byte lifetime = random(MIN_LIFETIME, MAX_LIFETIME);
+        byte lifetime = random(minLifetime, maxLifetime);
         ledLifetime[i] = lifetime;
         ledLifeLeft[i] = lifetime;
       } else {
@@ -84,3 +96,8 @@ void loop() {
 void sendColorAndBrightness(byte color[3], float brightness) {
   TCL.sendColor(color[0] * brightness, color[1] * brightness, color[2] * brightness);
 }
+
+byte getLifetimeMultiplier() {
+  return (analogRead(TCL_POT3) >> 2) / 3 + 1;
+}
+
