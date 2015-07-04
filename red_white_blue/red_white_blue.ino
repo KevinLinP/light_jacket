4439ea6bf97089a8c665d1259aef0bdf76894f49
Kevin Lin <kevin.lin.p@gmail.com>
2015-07-03 18:22:58 -0700

    red/white/blue: now flashing at random brightness.

--- red_white_blue/red_white_blue.ino
+++ red_white_blue/red_white_blue.ino
@@ -8,32 +8,31 @@ const int TOTAL_FRAMES = 270;
 const long INTERVAL = 1000 / FRAMES_PER_SECOND;
 int frame = 0;
 
+const float BRIGHTNESS = 0.01;
 byte FULL_RED[] = {255, 0, 0};
-byte FULL_WHITE[] = {220, 255, 255};
+byte FULL_WHITE[] = {255, 255, 255};
 byte FULL_BLUE[] = {0, 0, 255};
 
 long previousMillis = 0;
 
+byte red[3];
+byte white[3];
+byte blue[3];
+
+byte flashRed[3];
+byte flashWhite[3];
+byte flashBlue[3];
 
 void setup() {
   TCL.begin();
+  changeBrightness(FULL_RED, BRIGHTNESS, red);
+  changeBrightness(FULL_WHITE, BRIGHTNESS, white);
+  changeBrightness(FULL_BLUE, BRIGHTNESS, blue);
+  changeBrightness(FULL_WHITE, 0.33333, flashWhite);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-  
-  float brightness = 0.01;
-  
-  byte red[3];
-  byte white[3];
-  byte blue[3];
-  changeBrightness(FULL_RED, brightness, red);
-  changeBrightness(FULL_WHITE, brightness, white);
-  changeBrightness(FULL_BLUE, brightness, blue);
-
-  byte flashWhite[3];
-  changeBrightness(FULL_WHITE, 0.33333, flashWhite);
-  
   
   if (currentMillis - previousMillis > INTERVAL) {
     previousMillis = currentMillis;
@@ -44,21 +43,22 @@ void loop() {
     
     int n;
     for(n=0; n<LEDS; n++) {
+      int i = n + color;
+      
       if(random(100000) < 250) {
         byte randomBrightness = random(5, 11);
-        float brightness = randomBrightness / 10;
-        int i = n + color;
-        
-  
+        float brightness = randomBrightness / 10.0;
         if (i % 3 == 0) {
-          sendColor(FULL_RED);
+          changeBrightness(FULL_RED, brightness, flashRed);
+          sendColor(flashRed);
         } else if (i % 3 == 1) {
+          changeBrightness(FULL_WHITE, brightness, flashWhite);
           sendColor(flashWhite);
         } else {
-          sendColor(FULL_BLUE);
+          changeBrightness(FULL_BLUE, brightness, flashBlue);
+          sendColor(flashBlue);
         }
       } else {
-        int i = n + color;
         if (i % 3 == 0) {
           sendColor(red);
         } else if (i % 3 == 1) {
