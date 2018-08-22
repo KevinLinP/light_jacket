60da5eefded5b4d16c3307c83dcb000061ccbd42
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-21 22:30:07 -0700

    adding rotatingRainbow()

--- rainbow_v2/rainbow_v2.ino
+++ rainbow_v2/rainbow_v2.ino
@@ -25,12 +25,14 @@ void setup() {
 
 void loop() {
   FastLED.show();
+  
   setBrightness();
   setSpeed();
   setSpread();
   
   frameCounter++;
-  sweepingRainbow();
+  rotatingRainbow();
+  //sweepingRainbow();
   transfer2dPixelsToLinear();
 
   FastLED.delay(INTERVAL);
@@ -73,11 +75,60 @@ void setSpread() {
 
 void sweepingRainbow() {
   uint8_t startHue = (frameCounter % (256 * speedFactor)) / speedFactor;
-  for (int y = 0; y < Y_LENGTH; y++) {
+  for (uint8_t y = 0; y < Y_LENGTH; y++) {
     uint8_t thisStartHue = startHue + (spread * y);
     fill_rainbow(leds[y], X_LENGTH, thisStartHue, spread);
   }
 }
+
+void rotatingRainbow() {
+  uint8_t angle = (frameCounter % 1024) / 4;
+  uint8_t startHue = (frameCounter % 512) / 2;
+  uint8_t deltaDivisor = 8;
+  /*startHue = 0;*/
+  uint8_t xHueDelta = triwave8(angle); // why the F triwave works better, i'm not sure.
+  /*xHueDelta = 0;*/
+  uint8_t yHueDelta = triwave8(angle) + 128;
+  /*yHueDelta = 0;*/
+  
+  // i'm sure when someone tells me the easier way to do this i'll be embarassed
+  if (yHueDelta >= 128) {
+    yHueDelta = 255 - yHueDelta;
+  }
+  yHueDelta /= deltaDivisor;
+  
+  if (xHueDelta >= 128) {
+    xHueDelta = 255 - xHueDelta;
+  }
+  xHueDelta /= deltaDivisor;
+
+  CHSV rowStartColors[Y_LENGTH];
+
+  if (angle >= 64 && angle <= 191) {
+    fill_rainbow(rowStartColors, Y_LENGTH, startHue, yHueDelta);
+  } else {
+    CHSV columnToReverse[Y_LENGTH];
+    fill_rainbow(columnToReverse, Y_LENGTH, startHue, yHueDelta);
+    for (uint8_t i = 0; i < Y_LENGTH; i++) {
+      rowStartColors[Y_LENGTH - 1 - i] = columnToReverse[i];
+    }
+  }
+
+  for (int y = 0; y < Y_LENGTH; y++) {
+    CHSV rowStartColor = rowStartColors[y];
+
+    if (angle <= 127) {
+      fill_rainbow(leds[y], X_LENGTH, rowStartColor.hue, xHueDelta);
+    } else {
+      CHSV rowToReverse[X_LENGTH];
+      fill_rainbow(rowToReverse, X_LENGTH, rowStartColor.hue, xHueDelta);
+      for (uint8_t i = 0; i < X_LENGTH; i++) {
+        leds[y][X_LENGTH - 1 - i] = rowToReverse[i];
+      }
+    }
+  }
+}
+
 
 // based on my light jacket's LED arrangement
 void transfer2dPixelsToLinear() {
